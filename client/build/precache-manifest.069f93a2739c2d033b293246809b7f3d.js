187ac0b742758a1dd384f9456c12cc78d5a4791b
Daniel Palencia <Lionheart920@hotmail.com>
2020-11-03 10:41:21 -0800

    ready for deploy

---
+++ client/build/precache-manifest.069f93a2739c2d033b293246809b7f3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05bc2f4651c4898d194f82abcb3dafc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "295c965a574bb1fa4590",
+    "url": "/static/css/main.e37a13fc.chunk.css"
+  },
+  {
+    "revision": "f58dc51715c9ea1ad958",
+    "url": "/static/js/2.b7143e34.chunk.js"
+  },
+  {
+    "revision": "295c965a574bb1fa4590",
+    "url": "/static/js/main.a0207483.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
